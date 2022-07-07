477962dd7145989f4fd7ee88a92e9bf090be062a
martingferraro <martingferraro@gmail.com>
2022-07-07 17:51:47 -0300

    datefield

--- Appfamiliares/models.py
+++ Appfamiliares/models.py
@@ -7,4 +7,6 @@ class Familiar (models.Model):
     nombre= models.CharField(max_length=30)
     apellido= models.CharField(max_length=30)
     edad= models.IntegerField ()
+    fecha_nacimiento= models.DateField()
+    
 
--- Appfamiliares/views.py
+++ Appfamiliares/views.py
@@ -9,21 +9,21 @@ from django.template import Context, context, Template
 
 
 def padre(self):
-    padre= Familiar (nombre= "Juan", apellido="Perez", edad= "70")
+    padre= Familiar (nombre= "Juan", apellido="Perez", edad= "70", fecha_nacimiento="1952-11-15")
     padre.save()
-    texto= f"Familiar creado. Nombre: {padre.nombre}, Apellido: {padre.apellido}, Edad: {padre.edad} "
+    texto= f"Familiar creado. Nombre: {padre.nombre}, Apellido: {padre.apellido}, Edad: {padre.edad}, Fecha de nacimiento: {padre.fecha_nacimiento} "
     return HttpResponse (texto)
 
 def madre(self):
-    madre= Familiar (nombre= "Ana", apellido="Sosa", edad= "60")
+    madre= Familiar (nombre= "Ana", apellido="Sosa", edad= "60", fecha_nacimiento="1962-10-26")
     madre.save()
-    texto= f"Familiar creado. Nombre: {madre.nombre}, Apellido: {madre.apellido}, Edad: {madre.edad} "
+    texto= f"Familiar creado. Nombre: {madre.nombre}, Apellido: {madre.apellido}, Edad: {madre.edad}, Fecha de nacimiento: {madre.fecha_nacimiento} "
     return HttpResponse (texto)
 
 def hermano(self):
-    hermano= Familiar (nombre= "Andres", apellido="Perez", edad= "30")
+    hermano= Familiar (nombre= "Andres", apellido="Perez", edad= "30", fecha_nacimiento="1992-02-10")
     hermano.save()
-    texto= f"Familiar creado. Nombre: {hermano.nombre}, Apellido: {hermano.apellido}, Edad: {hermano.edad} "
+    texto= f"Familiar creado. Nombre: {hermano.nombre}, Apellido: {hermano.apellido}, Edad: {hermano.edad}, Fecha de nacimiento: {hermano.fecha_nacimiento}"
     return HttpResponse (texto)
 
 def padre_template(self):
@@ -32,8 +32,9 @@ def padre_template(self):
     nombre1= "Juan"
     Apellido= "Perez"
     Edad= "70"
+    fecha= "1952-11-15"
 
-    diccionario= {"nombre": nombre1, "apellido": Apellido, "Edad": Edad }
+    diccionario= {"nombre": nombre1, "apellido": Apellido, "Edad": Edad, "fecha": fecha }
 
     plantilla= Template(mi_archivo.read())
     mi_archivo.close()
@@ -49,8 +50,9 @@ def madre_template(self):
     nombre1= "María"
     Apellido= "Sosa"
     Edad= "60"
+    fecha= "1962-10-25"
 
-    diccionario= {"nombre": nombre1, "apellido": Apellido, "Edad": Edad }
+    diccionario= {"nombre": nombre1, "apellido": Apellido, "Edad": Edad , "fecha": fecha }
 
     plantilla= Template(mi_archivo.read())
     mi_archivo.close()
@@ -65,9 +67,10 @@ def hermano_template(self):
 
     nombre1= "Andres"
     Apellido= "Ferraro"
-    Edad= "40"
+    Edad= "30"
+    fecha= "1992-05-15"
 
-    diccionario= {"nombre": nombre1, "apellido": Apellido, "Edad": Edad }
+    diccionario= {"nombre": nombre1, "apellido": Apellido, "Edad": Edad , "fecha": fecha}
 
     plantilla= Template(mi_archivo.read())
     mi_archivo.close()
